63d0eb05fe4afed04b87a03ce36db0392fddc303
Github <no-reply@github.com>
2023-10-17 10:19:24 +0000

    Update documentation for v0.2
    Auto-generated from f90ee4c by 'update-gh-pages.sh'

--- versions.js
+++ versions.js
@@ -2,5 +2,7 @@ function getVersionsMenuItems() {
   return [
     { name: 'devel', url: '/nri-plugins/devel' },
     { name: 'main', url: '/nri-plugins/main' },
+    { name: 'stable', url: '/nri-plugins/stable' },
+    { name: 'v0.2', url: '/nri-plugins/v0.2' },
   ];
 }
