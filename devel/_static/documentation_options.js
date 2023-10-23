0db80a4f2daa30d608fcb42ca228ff405c9fc508
Github <no-reply@github.com>
2023-10-23 11:08:21 +0000

    Update documentation for devel
    Auto-generated from 177d80e by 'update-gh-pages.sh'

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.0-397-gd4a4234',
+    VERSION: 'v0.0.0-397-g177d80e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
