b186746f99d02589ba47c56d6ad59f814433c7a3
Github <no-reply@github.com>
2024-02-16 09:43:55 +0000

    Update documentation for devel
    Auto-generated from v0.3.1-68-gd605d28 by 'update-gh-pages.sh'

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.3.1-68-g5d220c8',
+    VERSION: 'v0.3.1-68-gd605d28',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
