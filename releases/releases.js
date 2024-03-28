7ce35d8be0ecc320ca9a567264c890044e76f158
Github <no-reply@github.com>
2024-03-28 12:37:37 +0000

    Update documentation for v0.5
    Auto-generated from v0.5.0-devel by 'update-gh-pages.sh'

--- releases/releases.js
+++ releases/releases.js
@@ -1,4 +1,5 @@
 function getReleaseListItems() {
   return [
+    { name: 'v0.5.0-devel', url: 'v0.5.0-devel' },
   ];
 }
--- versions.js
+++ versions.js
@@ -5,5 +5,6 @@ function getVersionsMenuItems() {
     { name: 'stable', url: '/nri-plugins/stable' },
     { name: 'v0.2', url: '/nri-plugins/v0.2' },
     { name: 'v0.4', url: '/nri-plugins/v0.4' },
+    { name: 'v0.5', url: '/nri-plugins/v0.5' },
   ];
 }
