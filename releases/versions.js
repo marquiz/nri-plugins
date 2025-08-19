6f5be876356c3db049c749d845ecd0fe881a4ba6
Github <no-reply@github.com>
2025-08-19 12:47:44 +0000

    Update documentation for v0.10
    Auto-generated from v0.5.0-devel-402-g502f406 by 'update-gh-pages.sh'

--- releases/versions.js
+++ releases/versions.js
@@ -3,6 +3,7 @@ function getVersionsMenuItems() {
     { name: 'devel', url: '/nri-plugins/devel' },
     { name: 'main', url: '/nri-plugins/main' },
     { name: 'stable', url: '/nri-plugins/stable' },
+    { name: 'v0.10', url: '/nri-plugins/v0.10' },
     { name: 'v0.2', url: '/nri-plugins/v0.2' },
     { name: 'v0.4', url: '/nri-plugins/v0.4' },
     { name: 'v0.5', url: '/nri-plugins/v0.5' },
